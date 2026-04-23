29ef2540a33af14683ec831440dc0a1a8e6e86ea
BagarBilla <piyushpra33@gmail.com>
2026-04-23 15:45:56 +0530

    cors updated

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(helmet());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://hirisionn.com",
     credentials: true,
 }));
 app.use(cookieParser());
